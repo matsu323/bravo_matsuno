d843e2409a2a350d104bf7a741bc58e5c596ce18
poteti1919 <hatawaniwani24@gmail.com>
2023-01-01 01:42:59 +0900

    Home画面をデザインしました

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,19 @@
 import React from "react"
-import {Container, Typography} from "@mui/material";
+import {Button, Container, Typography, Box} from "@mui/material";
+import "../assets/styles/BackgroundGradation.css"
 
 const Home = () => {
     return (<>
-        <Container>
-            <Typography>Hello</Typography>
+        <Container className="App">
+            <Typography variant="h1" fontFamily={'"Helvetica Neue", sans-serif'}>T2CALENDAR</Typography>
+            <Box sx={{height:60}}/>
+            <Button href="">MY CALENDAR</Button>
+            <Box sx={{height:20}}/>
+            <Button href="/user/login">LOGIN</Button>
+            <Box sx={{height:20}}/>
+            <Button href="/user/login">LOGOUT</Button>
+            <Box sx={{height:20}}/>
+            <Button href="/user/login">NEW ACCOUNT</Button>
         </Container>
     </>)
 }
